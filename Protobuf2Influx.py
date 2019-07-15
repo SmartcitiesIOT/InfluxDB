03247fa4859cfa6b18f32411a22fc8a636749d17
pavithranp <ptpn94@gmail.com>
2019-07-16 00:57:49 +0200

    Added Pddl Planner and Publish

--- Protobuf2Influx.py
+++ Protobuf2Influx.py
@@ -4,25 +4,84 @@ import logging
 import time
 import pickle
 import SensorMsg_pb2
+import ActuatorMsg_pb2
 import google.protobuf
+import urllib2, sys
+import re
 
 from influxdb import InfluxDBClient
 from datetime import datetime
 
-MQTT_BROKER_IP = "192.168.0.103" 
+MQTT_BROKER_IP = "192.168.0.107" 
 MQTT_BROKER_PORT = 1883
 MQTT_KEEPALIVE_INTERVAL = 45
 MQTT_TOPIC = "base"
 
+Sensor_Read = SensorMsg_pb2.SensorData()
+Actuator = ActuatorMsg_pb2.ActuatorData()
+Actuator2 = ActuatorMsg_pb2.ActuatorData()
 #Implementation of Callback methods
 def on_connect(client, userdata, flags, rc) :
     print ("InfluxDB is connected to MQTT Broker !!")
     #client.subscribe(MQTT_TOPIC)
 
+def update_problem_pddl():
+    problem_pddl_str=""
+    with open("p01.pddl") as fin:
+        problem_pddl_str = fin.read()
+    temperature_str = "(= (temperature r1) " + str(Sensor_Read.Temperature) +")"
+    humidity_str = "(= (humidity r1) " + str(Sensor_Read.RelativeHumidity) +")"
+    luminance_str = "(= (luminance r1) " + str(Sensor_Read.Luminance) +")"
+    uv_str = "(= (uv r1) " + str(Sensor_Read.Ultraviolet) +")" 
+    if (Sensor_Read.Motion == "True"):
+        motion_str = "(= (motion r1) " + str(1) +")"
+    else:
+        motion_str = "(= (motion r1) " + str(0) +")"
+    problem_pddl_str =  re.sub('\(= \(temperature r1\) [0-9.]+\)', temperature_str, problem_pddl_str)
+    problem_pddl_str =  re.sub('\(= \(humidity r1\) [0-9.]+\)', humidity_str, problem_pddl_str)
+    problem_pddl_str =  re.sub('\(= \(luminance r1\) [0-9.]+\)', luminance_str, problem_pddl_str)
+    problem_pddl_str =  re.sub('\(= \(uv r1\) [0-9.]+\)', uv_str, problem_pddl_str)
+    problem_pddl_str =  re.sub('\(= \(motion r1\) [0-9.]+\)', motion_str, problem_pddl_str)
+    
+    with open("p01.pddl", "w") as fout:
+        fout.write(problem_pddl_str)
+
+def get_pddl_plan():
+    # Get plan from online planner
+    # Publish to mqtt
+    data = {'domain': open("domain.pddl", 'r').read(),
+        'problem': open("p01.pddl", 'r').read()}
+
+    req = urllib2.Request('http://solver.planning.domains/solve')
+    req.add_header('Content-Type', 'application/json')
+    resp = json.loads(urllib2.urlopen(req, json.dumps(data)).read())
+
+    return ('\n'.join([act['name'] for act in resp['result']['plan']]))
+
+def publish_pddl_plan(actuator_plan):
+    #if ("turn-on-hvac" in actuator_plan): 
+     #   Actuator.hvac_on = 1; 
+    #else: 
+     #   Actuator.hvac_on = 0;
+
+   # if ("turn-on-light" in actuator_plan):
+    #    Actuator.light_on = 1;
+   # else:
+    #    Actuator.light_on = 0;
+    
+    MSG=pickle.dumps(actuator_plan) 
+
+    #MSG = Actuator.SerializeToString()
+    #print("Actuator : " , Actuator.hvac_on)
+    #Actuator2.ParseFromString(MSG)
+    #print("Actuator2 : " , Actuator2.hvac_on)
+    mqtt_client.publish("actuate",MSG,qos=0, retain=True)
+    print("Published Actuator Plan")
+
 
 def on_message(client, userdata, msg):
     #print(pickle.loads(msg.payload))
-    Sensor_Read = SensorMsg_pb2.SensorData()
+    #Sensor_Read = SensorMsg_pb2.SensorData()
     #`print (str(msg.payload))
     Sensor_Read.ParseFromString(msg.payload)
     #print (str(Sensor_Read.last_updated).replace("\n", " ")) 
@@ -57,6 +116,12 @@ def on_message(client, userdata, msg):
     print("Dinesh" + str(json_body))
     client_influx.write_points(json_body)
     print ("Wrote values to DB")
+    update_problem_pddl()
+    print ("Problem Updated")
+    pddl_plan = get_pddl_plan()
+    publish_pddl_plan(pddl_plan)
+    print (pddl_plan)
+
 
 client_influx = InfluxDBClient('localhost', 8086, 'root', 'root', 'Sensor_Values')
 client_influx.create_database('Sensor_Values')
